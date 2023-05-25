6ee19b65738ea0c151ecfaa841724e7187a03584
Astromin21 <docgit@gmail.com>
2023-05-25 10:47:50 +0200

    html und css erstellt

--- main.js
+++ main.js
@@ -5,73 +5,151 @@ function computerPlay() {
   let computerAuswahl;
   if (zahlen === 0) {
     computerAuswahl = "schere";
+    ausgabeCom('schere');
   } else if (zahlen === 1) {
     computerAuswahl = "stein";
+    ausgabeCom('stein');
   } else {
     computerAuswahl = "papier";
+    ausgabeCom('papier');
   }
 
   return computerAuswahl;
 }
 
-function playRound() {
-  let spielerEingabe = prompt("Wählen Sie zwischen Schere, Stein oder Papier!");
-  let spielerAuswahl = spielerEingabe.toLocaleLowerCase();
+function playRound(pChoice) {
+  let spielerAuswahl = pChoice;
   let computerSelect = computerPlay();
+  console.log(computerSelect);
+  console.log("spieler", spielerAuswahl)
   let ergebnis;
   if (computerSelect === "schere" && spielerAuswahl === "schere") {
     ergebnis = 0;
-    return ergebnis;
   } else if (computerSelect === "schere" && spielerAuswahl === "stein") {
     ergebnis = 2;
-    return ergebnis;
+
   } else if (computerSelect === "schere" && spielerAuswahl === "papier") {
     ergebnis = 1;
-    return ergebnis;
+
   } else if (computerSelect === "stein" && spielerAuswahl === "stein") {
     ergebnis = 0;
-    return ergebnis;
+
   } else if (computerSelect === "stein" && spielerAuswahl === "schere") {
-    ergebnis = 2;
-    return ergebnis;
-  } else if (computerSelect === "stein" && spielerAuswahl === "papier") {
     ergebnis = 1;
-    return ergebnis;
+
+  } else if (computerSelect === "stein" && spielerAuswahl === "papier") {
+    ergebnis = 2;
+
   } else if (computerSelect === "papier" && spielerAuswahl === "papier") {
     ergebnis = 0;
-    return ergebnis;
+
   } else if (computerSelect === "papier" && spielerAuswahl === "schere") {
     ergebnis = 2;
-    return ergebnis;
+
   } else if (computerSelect === "papier" && spielerAuswahl == "stein") {
     ergebnis = 1;
-    return ergebnis;
+
+  }
+  rundensieg(ergebnis);
+  points(ergebnis);
+}
+
+function rundensieg(sieger){
+  let message = document.querySelector('#meldung');
+  if(sieger == 2) {
+    message.innerHTML = "Spieler gewinnt"
+  } else if (sieger == 1) {
+    message.innerHTML = "Computer gewinnt!"
+  } else {
+    message.innerHTML = "Unentschieden"
+  }
+}
+function points(sieger){
+  let comp = document.querySelector('#comP');
+  let spieler = document.querySelector('#spielerP');
+  if(sieger == 2){
+    player++;
+  }
+  else if(sieger == 1){
+    com++;
+  }
+  spieler.innerHTML = player;
+  comp.innerHTML = com;
+  sieg();
+
+}
+
+function sieg(){
+  let message = document.querySelector('#meldung');
+  if(player == 5){
+    message.innerHTML = "Spieler gewinnt das Spiel"
+  } else if(com == 5){
+    message.innerHTML = "Computer gewinnt das Spiel!"
   }
 }
 
-function game() {
-  let computer = 0;
-  let spieler = 0;
-  let unentschieden = 0;
-  for (let i = 0; i < 5; i++) {
-    let spiele = playRound();
+function spiel() {
+  const schere = document.querySelector('#schere')
+  const papier = document.querySelector('#papier')
+  const stein = document.querySelector('#stein')
+  schere.addEventListener('click', () => {
+    playRound('schere');
+    ausgabe('schere');
 
-    if (spiele === 1) {
-      computer++;
-      console.log("Der Computer hat dieses mal gewonnen!");
-    } else if (spiele === 2) {
-      spieler++;
-      console.log("Der Spieler hat dieses mal gewonnen!");
-    } else if (spiele === 0) {
-      unentschieden++;
-      console.log("Dieses Spiel ist unentschieden!");
-    }
+  })
+  papier.addEventListener('click', () => {
+    playRound('papier');
+    ausgabe('papier');
+
+  })
+  stein.addEventListener('click', () => {
+    playRound('stein');
+    ausgabe('stein');
+
+  })
+}
+
+function ausgabe(zeichen) {
+  let message = document.querySelector('#spielerA');
+
+  if (zeichen == 'schere') {
+    message.innerHTML = "Schere";
   }
-  if (computer > spieler) {
-    console.log("Der Computer gewinnt mit " + String(computer) + " Punkten!");
-  } else if (spieler < computer) {
-    console.log("Der Spieler gewinnt mit " + String(spieler) + " Punkten!");
-  } else if (unentschieden > spieler && unentschieden > computer) {
-    console.log("Das Spiel ist unentschieden!");
+
+  else if (zeichen == 'papier') {
+    message.innerHTML = "Papier";
+  }
+  else {
+    message.innerHTML = "Stein";
   }
 }
+function ausgabeCom(zeichencom) {
+  let message = document.querySelector('#comA');
+  if (zeichencom == 'schere') {
+    message.innerHTML = "Schere";
+  }
+
+  else if (zeichencom == 'papier') {
+    message.innerHTML = "Papier";
+  }
+  else {
+    message.innerHTML = "Stein";
+  }
+}
+
+function sieg(sieger) {
+  if (sieger == 5) {
+    console.log("Der Spieler hat gewonnen")
+  }
+  else if ( sieger == -5){
+    console.log(("Der Computer hat gewonnen"));
+  }
+}
+function meldung(){
+  let message = document.querySelector('#meldung');
+
+}
+
+let player = 0;
+let com = 0;
+spiel();
